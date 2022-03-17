d8b7591c9b0f182bce3110589d4260d692066130
Marine <marine.lenoir@hotmail.fr>
2022-03-17 10:48:34 +0100

    version JS non condensé étape 2

--- ScriptJSAllumettes.js
+++ ScriptJSAllumettes.js
@@ -1,22 +1,24 @@
 let match = 50;
 
+
 function Player1(){
 
    while(match > 0){
-   let question = parseInt(prompt("Choisis un nombre d'allumettes entre 1 et 6"));
+   let question = parseInt(prompt("Joueur 1: Choisis un nombre d'allumettes entre 1 et 6"));
    console.log(question);
    
     if(question < 1 || question > 6){
         alert("Erreur: Choisis un nombre entre 1 et 6");
-        askANumber(); 
+        Player1(); 
     }
     if(match < question){
         alert("Erreur: Choisis un nombre plus petit");
-        askANumber();
+        Player1();
      }   
     else{
         match = match - question;
         console.log(match);
+        Player2();
      }
     if(match == 0){
         alert("Bravo tu as gagné!"); 
@@ -27,5 +29,34 @@ function Player1(){
    }
 }
 
+function Player2(){
+
+    while(match > 0){
+    let question = parseInt(prompt("Joueur 2: Choisis un nombre d'allumettes entre 1 et 6"));
+    console.log(question);
+    
+     if(question < 1 || question > 6){
+         alert("Erreur: Choisis un nombre entre 1 et 6");
+         Player2(); 
+     }
+     if(match < question){
+         alert("Erreur: Choisis un nombre plus petit");
+         Player2();
+      }   
+     else{
+         match = match - question;
+         console.log(match);
+         Player1();
+      }
+     if(match == 0){
+         alert("Bravo tu as gagné!"); 
+         //Réinitialise match à 50 avant de pouvoir rappeller la fonction askANumber() et relancer une partie//
+         match = 50,
+         Player2();
+      } 
+    }
+ }
+
 Player1();
+Player2();
 
